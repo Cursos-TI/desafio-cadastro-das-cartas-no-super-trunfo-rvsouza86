a7fa624fc0b3d558c1c154effde9542429c03fa5
Robert V S Ricardo <robertsouzaricardo18@gmail.com>
2025-03-27 01:44:33 +0000

    codigonovato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,64 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    char Carta1;
+
+    char estado1;  
+
+    char carta1[12];  
+
+    char cidade1[50];     
+
+    int populacao1 ;  
+    
+    float area1;  
+
+    float PIB1;   
+
+    int  pontosturisticos1 ; 
+
+    
+    
+    //os atributos da carta são preenchidos pelo jogador
+    
+    
+
+    printf("Carta 1:\n");
+
+    printf("Digite uma letra entre 'A' e 'H' para representar um estado: ");
+    scanf("%c" , &estado1);
+
+    printf("codigo da carta: ");
+    scanf("%s", &carta1);
+   
+   printf("Qual a cidade:");
+   scanf("%s", &cidade1);
+   
+    printf("Quantidade de habitantes:");
+    scanf("%d", &populacao1);
+
+    printf("tamanho da cidade em Km²:");
+    scanf("%f", &area1);
+
+    printf("Qual o PIB da cidade:");
+    scanf("%f", &PIB1);
+    
+    printf("Pontos turísticos da sua cidade:");
+    scanf("%d", &pontosturisticos1);      
+
+
+
+    printf("Carta 1:%c\n", Carta1);
+    printf("Estado: %c\n ", estado1);
+    printf("Código:  %s\n", carta1);
+    printf("Cidade: %s\n", cidade1);
+    printf("População: %d habitantes\n", populacao1);
+    printf("Àrea: %f  km²\n", area1);
+    printf("PIB: %f  bilhões de reais\n", PIB1);
+    printf("Pontos turísticos: %d\n", pontosturisticos1);
+
+    
 
     return 0;
 }
